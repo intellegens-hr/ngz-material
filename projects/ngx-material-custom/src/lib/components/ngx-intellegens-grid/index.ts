6d0407d2e19e67762a4bba2e0389d342ae4448e0
mrAsabol <andrej.sabol@intellegens.hr>
2020-01-17 14:29:15 +0100

    intellegens-grid first changes

--- projects/ngx-material-custom/src/lib/components/ngx-intellegens-grid/index.ts
+++ projects/ngx-material-custom/src/lib/components/ngx-intellegens-grid/index.ts
@@ -2,13 +2,62 @@
 // ----------------------------------------------------------------------------
 
 // Import dependencies
-import { Component } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
+import { isObservable, throwError } from 'rxjs';
+import { isPromise } from '@angular/compiler/src/util';
+
 
 @Component({
   selector: 'ngx-intellegens-grid',
   templateUrl: './index.html',
   styleUrls: ['./style.scss']
 })
+
 export class NgxIntellegensGridComponent {
 
+  @Input()
+  public dataSource: any;
+
+  @Input()
+  public error?: any;
+
+  @Input()
+  public loading?: boolean;
+
+  private resolvedDataSource: any = [];
+  public get resolvedDataSourceKeys () {
+    return (this.resolvedDataSource && this.resolvedDataSource.length ? Object.keys(this.resolvedDataSource[0]) : []);
+  }
+
+  public ngOnChanges () {
+     if (isPromise(this.dataSource)) {
+       // TODO: use async/await
+      this.dataSource.then(
+        (dataSource) => {
+          this.resolvedDataSource = dataSource;
+        },
+        (err) => {
+          this.handleError(err);
+        }
+      );
+    } else if (isObservable(this.dataSource)) {
+      this.dataSource.subscribe(
+        (dataSource) => {
+          this.resolvedDataSource = dataSource;
+        },
+        (err) => {
+          this.handleError(err);
+        }
+      );
+    } else {
+      this.resolvedDataSource = this.dataSource;
+    }
+  }
+
+  public handleError (error) {
+
+  }
+
 }
+
+
--- projects/showcase/src/showcases/ngx-intellegens-grid-showcase/index.ts
+++ projects/showcase/src/showcases/ngx-intellegens-grid-showcase/index.ts
@@ -3,11 +3,64 @@
 
 // Import dependencies
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './index.html',
   styleUrls: ['./style.scss']
 })
 export class NgxIntellegensGridShowcaseComponent {
+
+  public testingError: Error = null;
+  public testingLoading = false;
+
+  public dataSource = [
+    { location: 1, firstName: 'Marko', lastName: 'Horvat', salary: 2000 },
+    { location: 2, firstName: 'Ivo', lastName: 'Balija', salary: 1500 },
+    { location: 3, firstName: 'Martina', lastName: 'Posavec', salary: 3753 },
+    { location: 4, firstName: 'Marko', lastName: 'Horvat', salary: 1700 },
+  ];
+
+
+  public dataSourcePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = JSON.parse(JSON.stringify(this.dataSource));
+      resolve(data);
+      //reject(new Error('I am an error'));
+    }, 1000);
+  });
+
+  public dataSourceObservable = new Observable((subscriber) => {
+    let count = 0,
+        interval = setInterval(() => {
+          if (count < 10) {
+            const data = JSON.parse(JSON.stringify(this.dataSource));
+            subscriber.next(
+              this.updateDataSource(data, count)
+            );
+            count++;
+          } else {
+            subscriber.complete();
+            clearInterval(interval);
+            // subscriber.error((new Error('I am an error')));
+            // clearInterval(interval);
+          }
+        }, 1000);
+  });
+
+
+  public updateDataSource (dataSource, i) {
+    for (let row of dataSource) {
+      row.location += i;
+    }
+    return dataSource;
+  }
+
+  public toggleError () {
+    this.testingError = (this.testingError ? null : new Error('Really, I am an error!'));
+  }
+  public toggleLoading () {
+    this.testingLoading = !this.testingLoading;
+  }
 
 }
