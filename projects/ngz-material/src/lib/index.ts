5c6e6231c08fe6546fc3c2750882dac4c2301046
Juraj Gemić <ofzza@ofzza.com>
2020-05-07 00:18:24 +0200

    Fixed issues with IVY production compilation and es2015 targeting

--- projects/ngz-material/src/lib/index.ts
+++ projects/ngz-material/src/lib/index.ts
@@ -5,26 +5,31 @@
 import { NgModule } from '@angular/core';
 
 // Import and (re)export modules
+export * from './components/grid';
+export { GridModule } from './components/grid'; // Required IVY hinting
 import { GridModule } from './components/grid';
-export { GridModule } from './components/grid';
 
+export * from './components/modal';
+export { ModalModule }  from './components/modal'; // Required IVY hinting
 import { ModalModule } from './components/modal';
-export { ModalModule } from './components/modal';
 
+export * from './directives/a11n';
+export { A11nModule } from './directives/a11n'; // Required IVY hinting
 import { A11nModule } from './directives/a11n';
-export { A11nModule } from './directives/a11n';
 
 const modules = [
   GridModule, ModalModule, A11nModule
 ];
 
 // Import and (re)export services
+export * from './services';
+export { EnTTValidationMessagesService } from './services'; // Required IVY hinting
 import { EnTTValidationMessagesService } from './services';
-export { EnTTValidationMessagesService } from './services';
 
 // Import and (re)export pipes
+export * from './pipes';
+export { EnTTValidationMessagePipe, EnTTValidationMessagesPipe } from './pipes'; // Required IVY hinting
 import { EnTTValidationMessagePipe, EnTTValidationMessagesPipe } from './pipes';
-export { EnTTValidationMessagePipe, EnTTValidationMessagesPipe } from './pipes';
 const pipes = [
   EnTTValidationMessagePipe,
   EnTTValidationMessagesPipe
