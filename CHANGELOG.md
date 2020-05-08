fe68dd606ce375b8e810ab452b6c3f1fc915ed1d
Juraj Gemić <ofzza@ofzza.com>
2020-05-08 15:50:02 +0200

    Updated package.json info and publishing script; Added CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+#### v1.0.3
+
+- Added CHANGELOG.md
+- Updated package description, repository, keywords, license and author for publishing
+
+## v1.0.2, Stable version
+
+- NgzMaterialModule
+  - GridModule
+    - <ngz-grid /> component
+      - <ngz-grid-actions /> component
+      - <ngz-grid-action /> component
+  - ModalModule
+    - <ngz-modal /> component
+  - A11nModule
+    - [ngzFocus] directive
+  - other:
+    - EnTT validation messages
+      - EnTTValidationMessagesService service
+      - "enttValidationMessage" and "enttValidationMessages" pipes
--- scripts/publish-to-npm.sh
+++ scripts/publish-to-npm.sh
@@ -1,39 +1,78 @@
 #!/bin/bash
 cd "$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)";
 
+# Exit when any command fails
+set -e
+
 # Build library version
+echo '';
 echo '> Building latest library version ...';
-ng build --prod
+ng build --prod;
 
 # Check if main project package.json and library package.json are have same proeprty values
+echo '';
 echo '> Comparing package.json files between repo and library...';
-
+# Check version
 repVersion=$( cat ../package.json | jq -r ".version" );
 libVersion=$( cat ../dist/ngz-material/package.json | jq -r ".version");
 echo "- version: '${repVersion}' ?= '${libVersion}'";
 if [ "${repVersion}" != "${libVersion}" ]; then
-  echo "ERROR: versions in package.json are different between the showcase repo and the library!"
+  echo "ERROR: version in package.json is different between the showcase repo and the library!";
   exit 1;
 fi
-
+# Check name
 repName=$( cat ../package.json | jq -r ".name" );
 libName=$( cat ../dist/ngz-material/package.json | jq -r ".name");
 echo "- name: '${repName}' ?= '${libName}'";
 if [ "${repName}" != "${libName}" ]; then
-  echo "ERROR: names in package.json are different between the showcase repo and the library!"
+  echo "ERROR: name in package.json is different between the showcase repo and the library!";
   exit 1;
 fi
-
+# Check description
 repDescription=$( cat ../package.json | jq -r ".description" );
 libDescription=$( cat ../dist/ngz-material/package.json | jq -r ".description");
 echo "- description: '${repDescription}' ?= '${libDescription}'";
 if [ "${repDescription}" != "${libDescription}" ]; then
-  echo "ERROR: descriptions in package.json are different between the showcase repo and the library!"
+  echo "ERROR: description in package.json is different between the showcase repo and the library!";
+  exit 1;
+fi
+# Check repository
+repRepository=$( cat ../package.json | jq -r ".repository" );
+libRepository=$( cat ../dist/ngz-material/package.json | jq -r ".repository");
+echo "- repository: '${repRepository}' ?= '${libRepository}'";
+if [ "${repRepository}" != "${libRepository}" ]; then
+  echo "ERROR: repository in package.json is different between the showcase repo and the library!";
+  exit 1;
+fi
+# Check keywords
+repKeywords=$( cat ../package.json | jq -r ".keywords" );
+libKeywords=$( cat ../dist/ngz-material/package.json | jq -r ".keywords");
+echo "- keywords: '${repKeywords}' ?= '${libKeywords}'";
+if [ "${repKeywords}" != "${libKeywords}" ]; then
+  echo "ERROR: keywords in package.json are different between the showcase repo and the library!";
+  exit 1;
+fi
+# Check author
+repAuthor=$( cat ../package.json | jq -r ".author" );
+libAuthor=$( cat ../dist/ngz-material/package.json | jq -r ".author");
+echo "- keywords: '${repAuthor}' ?= '${libAuthor}'";
+if [ "${repAuthor}" != "${libAuthor}" ]; then
+  echo "ERROR: author in package.json is different between the showcase repo and the library!";
+  exit 1;
+fi
+# Check license
+repLicense=$( cat ../package.json | jq -r ".license" );
+libLicense=$( cat ../dist/ngz-material/package.json | jq -r ".license");
+echo "- license: '${repLicense}' ?= '${libLicense}'";
+if [ "${repLicense}" != "${libLicense}" ]; then
+  echo "ERROR: author in package.json is different between the showcase repo and the library!";
   exit 1;
 fi
 
-# TODO: Add checks for: repository, keywords, author, license
 
 # Publish via NPM
-cd ../dist/ngz-material
-npm publish --access public
+echo '';
+echo '> Publishing to NPM ...';
+cd ../dist/ngz-material;
+#npm publish --access public
+echo "PUBLISH NOW: publish, publish, publish ...";
